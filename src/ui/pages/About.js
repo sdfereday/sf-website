a5b7422c717c5ed5ed7dad722e06e05058997fed
Sam Fereday <sf230432@falmouth.ac.uk>
2022-06-06 11:04:28 +0100

    About page typo fix

--- src/ui/pages/About.js
+++ src/ui/pages/About.js
@@ -6,7 +6,10 @@ export default () => {
     <div className="lg:pt-8 pt-6 pb-6 max-w-5xl">
       <div class="lg:flex">
         <div class="flex-none pr-8 pb-8">
-          <img src={profileImage} className="object-cover h-48 w-48 rounded-md border-2 border-blue-500" />
+          <img
+            src={profileImage}
+            className="object-cover h-48 w-48 rounded-md border-2 border-blue-500"
+          />
         </div>
         <div class="grow">
           <h2>Who Am I?</h2>
@@ -16,14 +19,13 @@ export default () => {
             great user experience.
           </p>
           <p>
-            Eager to learn and a passion for overcoming challenges and problem
-            solving. When I'm not writing code or being creative you'll find me playing games (usually ones with swords),
-            practicing martial arts, adventuring outdoors or sitting peacefully
-            reading a book.
+            Eager to learn and have a passion for overcoming challenges and
+            problem solving. When I'm not writing code or being creative you'll
+            find me playing games (usually ones with dragons &amp; swords), practicing martial
+            arts, adventuring outdoors or sitting peacefully reading a book.
           </p>
           <p>
-            I'm always on the lookout to advance my knowledge to the next
-            level.
+            I'm always on the lookout to advance my knowledge to the next level.
           </p>
         </div>
       </div>
