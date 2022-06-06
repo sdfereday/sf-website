f994c4575882ebe25ed4b344971714dbfd893e90
Sam Fereday <sf230432@falmouth.ac.uk>
2022-06-06 11:00:25 +0100

    Updated homepage content

--- src/ui/pages/Home.js
+++ src/ui/pages/Home.js
@@ -5,8 +5,7 @@ export default () => {
     <div className="lg:pt-32 lg:pb-0 md:pt-48 pt-6 pb-6 max-w-3xl">
       <h2 className="text-6xl">Greetings!</h2>
       <p>
-        I'm Sam, a front-end web developer specializing in React, with skills
-        also ranging in Strapi, Mithril, Backbone, Tailwind, Bootstrap, Unity C#, Wordpress &amp; Umbraco.
+        I'm Sam, a front-end web developer specializing in React, with skills also ranging in Strapi, Mithril, Backbone, Tailwind, Bootstrap, Unity C#, Wordpress, Umbraco and a keen interest in creativity.
       </p>
     </div>
   );
